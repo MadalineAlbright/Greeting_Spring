4cf4db38131956362ee3e427f868f587a6d623b9
MadalineAlbright <fenesa111@gmail.com>
2022-05-04 21:14:38 +0300

    First STS commit

---
+++ src/main/java/com/example/practice/Greeting.java
@@ -0,0 +1,32 @@
+package com.example.practice;
+
+public class Greeting {
+	
+	private final long id;
+	private final String content;
+	
+	//create a constructor
+	
+	public Greeting(long id, String content) {
+	
+	    this.id = id;
+		this.content = content;
+	}
+	
+	//create a getters 
+	
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+
+	
+	
+	
+
+}
